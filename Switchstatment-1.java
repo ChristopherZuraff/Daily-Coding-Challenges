1ab9ef6bd586cdefe3692cd8533cb88d27d2ec7d
ChristopherZuraff <cmzuraff@yahoo.com>
2018-10-08 16:03:12 -0700

    Switch Statement

---
+++ Switchstatment-1.java
@@ -0,0 +1,27 @@
+public class Continents {
+	public static void main(String[] args) {
+
+		int continent = 1;
+    
+    switch(continent){
+      case 1: System.out.println("North America: Mexico city, Mexico");
+        break;
+      case 2: System.out.println("South America: Sao Paulo, Brazil");
+        break;
+      case 3: System.out.println("Europe: Moscow, Russia");
+        break;
+      case 4: System.out.println("Africa: Lagos, Nigiria");
+        break;
+      case 5: System.out.println("Asia: Shanghai, China");
+        break;
+      case 6: System.out.println("Australia: Sydney, Australia");
+        break;
+      case 7: System.out.println("Antarctica: McMurdo Station, US");
+        break;
+       default: System.out.println("Undefined continent");
+        break;
+               
+    }
+
+	}
+}
